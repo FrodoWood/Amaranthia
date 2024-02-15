e0ce14d0463cfcd4e569a11a6b687625791e5bb7
Jasraj Singh <elijahbaggins3@gmail.com>
2024-02-15 01:39:09 +0000

    Added new input system and navmesh. Player can move and rotates correctly.
    Using a test plane with scaled cubes as walls to test navmesh movement for player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.AI;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,4 +16,62 @@ public class PlayerController : MonoBehaviour
     [SerializeField] LayerMask clickableLayers;
 
     float lookRotationSpeed = 8f;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        input = new CustomActions();
+        AssignInputs();
+    }
+
+    private void AssignInputs()
+    {
+        input.Main.Move.performed += ctx => ClickToMove();
+    }
+
+    private void ClickToMove()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
+        {
+            agent.destination = hit.point;
+            if(clickEffect != null)
+            {
+                Instantiate(clickEffect, hit.point += new Vector3(0, 0.1f, 0), clickEffect.transform.rotation);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Update()
+    {
+        FaceTarget();
+        SetAnimations();
+    }
+    private void FaceTarget()
+    {
+        if(agent.velocity != Vector3.zero)
+        {
+            Vector3 direction = (agent.destination - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            //transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+            transform.rotation = lookRotation;
+        }
+    }
+
+    private void SetAnimations()
+    {
+        
+    }
+
 }
