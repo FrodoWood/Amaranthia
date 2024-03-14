cf4d1d9c7a06b89505ea712c1391ee23e3521ce9
Jasraj Singh <elijahbaggins3@gmail.com>
2024-03-14 17:41:31 +0000

    Added upgrades button to the UI
    working as intended except buttons remain there even when there's no upgrades in the pool

---
+++ Assets/Scripts/Upgrades/UpgradeButtonManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UpgradeButtonManager : MonoBehaviour
+{
+    public UpgradeManager upgradeManager;
+    public GameObject upgradeButtonPrefab;
+    public Transform buttonContainer;
+    public float buttonSpacing = 10f;
+    void Start()
+    {
+        float yOffset = 0f;
+        foreach (Upgrade upgrade in upgradeManager.upgradesPool)
+        {
+            GameObject newButton = Instantiate(upgradeButtonPrefab, buttonContainer);
+            Button button = newButton.GetComponent<Button>();
+
+            TextMeshProUGUI textButton = button.GetComponentInChildren<TextMeshProUGUI>();
+            textButton.text = upgrade.upgradeName;
+
+            RectTransform buttonRect = button.GetComponent<RectTransform>();
+            buttonRect.anchoredPosition = new Vector2(0f, yOffset);
+            yOffset -= (buttonRect.sizeDelta.y + buttonSpacing);
+
+            button.onClick.AddListener(() => upgradeManager.ActivateUpgrade(upgrade));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -14,10 +14,6 @@ public class UpgradeManager : MonoBehaviour
     }
     void Start()
     {
-        foreach (Upgrade upgrade in upgradesPool)
-        {
-            ActivateUpgrade(upgrade); ;
-        }
     }
 
     void Update()
