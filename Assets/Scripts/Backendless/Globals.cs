94cb2a6c9e5b09df66c91ea998d3cd485c22583e
Jasraj Singh <elijahbaggins3@gmail.com>
2024-04-13 22:47:17 +0100

    Implemented get and set high score for backendless
    Currently using buttons to get and print high score, and set random high score with a button

---
+++ Assets/Scripts/Backendless/Globals.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour
+{
+
+    public const string MENU_SCENE = "MainMenu";
+    public const string GAME_SCENE = "TestScene";
+    public const string BALL = "Ball";
+    public const string HIGH_SCORE_KEY = "HighScoreKey";
+    public const string APPLICATION_ID = "68B4536A-5D70-F52E-FFEF-679685358D00";
+    public const string REST_SECRET_KEY = "D6502ABA-082D-480B-9262-39484AFBA878";
+
+}
+
+
+
