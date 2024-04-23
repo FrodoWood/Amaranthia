ecd81e5a6f51fd3ffe26af6f3ce805ca7542acbb
Jasraj Singh <elijahbaggins3@gmail.com>
2024-04-23 10:46:15 +0100

    Upgrade UI almost working! Need to offset the cards, then add a few more stat upgrades.

--- Assets/Scripts/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using TMPro;
+using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour
 {
     public List<Upgrade> upgradesPool;
 
     private PlayerStats playerStats;
+    public Transform upgradeUIParent;
+    public GameObject upgradeUIPrefab;
 
     private void Awake()
     {
@@ -29,11 +33,33 @@ public class UpgradeManager : MonoBehaviour
     {
     }
 
+    [ContextMenu("Display upgrades")]
+    public void DisplayUpgradeChoices()
+    {
+        for(int i = 0; i<Mathf.Min(3, upgradesPool.Count); i++)
+        {
+            Upgrade upgrade = upgradesPool[i];
+            upgradesPool = upgradesPool.OrderBy(upgrade => Random.value).ToList();
+            GameObject upgradeUI = Instantiate(upgradeUIPrefab, upgradeUIParent);
+            upgradeUI.GetComponentInChildren<TextMeshProUGUI>().text = upgrade.GetName();
+            Vector3 newPosition = upgradeUIParent.position + Vector3.right * i * 100f;
+            upgradeUI.transform.position = newPosition;
+            upgradeUI.GetComponentInChildren<Button>().onClick.AddListener(() => ActivateUpgrade(upgrade));
+
+        }
+    }
+
     public void ActivateUpgrade(Upgrade upgrade)
     {
         if (!upgradesPool.Contains(upgrade)) return;
         if (upgrade.isActivated) return;
         upgrade.Initialise(playerStats);
         upgrade.isActivated = true;
+
+        for (int i = upgradeUIParent.childCount - 1; i >= 0; i--)
+        {
+            GameObject upgradeUIGameObject = upgradeUIParent.GetChild(i).gameObject;
+            Destroy(upgradeUIGameObject);
+        }
     }
 }
